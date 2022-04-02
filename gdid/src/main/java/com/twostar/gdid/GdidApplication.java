2826e0a99d9d039e993f40feca84c1225ac6f777
Doobyeol <doobyeol@gmail.com>
2022-04-03 01:04:01 +0900

    H2 DB 셋팅

---
+++ gdid/src/main/java/com/twostar/gdid/GdidApplication.java
@@ -0,0 +1,13 @@
+package com.twostar.gdid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdidApplication.class, args);
+	}
+
+}
---
+++ gdid/src/test/java/com/twostar/gdid/GdidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twostar.gdid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
